6542100143b2a81e39fa589e20a4e060948fc1d2
yuukubo <i.yusuke.shk@gmail.com>
2019-11-25 00:21:21 +0900

    set frame test

--- sketch.js
+++ sketch.js
@@ -1,37 +1,164 @@
 // stgv2
 
-let stgTitle = "* S T G * v2 c2.0"
-let [canvasx, canvasy] = [720, 690];
+// title
+  let stgTitle = "* S T G v2 * c3.2";
+
+// canvas
+  let [canvasW, canvasH] = [720, 720];
+//let [canvasW, canvasH] = [windowWidth - 20, windowWidth - 20];
+
+// game board
+  let [frameOriginX, frameOriginY, frameW, frameH] = [20, 20, 440, 680];
+  let bg_w = frameW;
+  let bg_h = 20;
+  let bg_spdy = 2;
+  let bg_x1 = frameOriginX;
+  let bg_y1 = frameOriginY;
+  let bg_x2 = frameOriginX;
+  let bg_y2 = frameOriginY - frameH;
+  let bg_num = 0;
+
+  // text area
+  let [textX, textY] = [480, 100];
+
+  let [textDifficultyX, textDifficultyY] = [540, 60];
+
+  let [textScoreX, textScoreY] = [textX, textY];
+  let [textZankiX, textZankiY] = [textX, textY + 40 * 1];
+  let [textBombX, textBombY] = [textX, textY + 40 * 2];
+  let [textPowerX, textPowerY] = [textX, textY + 40 * 3];
+  let [textGrazeX, textGrazeY] = [textX, textY + 40 * 4];
+
+  let [textTitleX, textTitleY] = [textX, textY + 40 * 6];
+
+  let [textFpsX, textFpsY] = [660, 700];
+
+  let [textSize12, textSize24, textSize36] = [12, 24, 36];
+
+// FPS
+  let fr = 0;
+
+// difficulty
+  let stgDifficulty = "HARD"
+
+//game instance
+  let game;
 
 function setup() {
   window.addEventListener("touchstart", function (event) { event.preventDefault(); }, { passive: false });
   window.addEventListener("touchmove", function (event) { event.preventDefault(); }, { passive: false });
-  createCanvas(canvasx, canvasy);
+  createCanvas(canvasW, canvasH);
+  fr = frameRate();
+  game = new Game();
+  bg_num = frameH / bg_h;
 }
 
 function draw() {
+  game.sceneCtl();
 }
 
 class Game {
   constructor() {
+    //game common
+    this.isDebug = false;
+    this.age = 0;
+    this.textInterval = 0;
+    this.textIntervalTimer = false;
+
+    //scene common
+    this.sceneStack = [];
+    this.gameSceneNow = "title";
+
+    //stage property
+    this.stage1Age = 0;
+    this.isStage1Clear = false;
+    this.isStage1BossNow = false;
+
+    this.isStage1prologueEnd = false;
+    this.stage1Prologueage = 0;
+    this.stage1PrologueCursor = 1;
+
+    this.stage2Age = 0;
+    this.isStage2Clear = false;
+    this.isStage2BossNow = false;
+
+    this.isNeedSetup = true;
+
+    this.totalScore = 0;
+    this.stage1Score = 0;
+    this.stage2Score = 0;
+
+    this.titleAlpha = 0;
+    this.intro1Alpha = 0;
+    this.intro2Alpha = 0;
+    this.endingAlpha = 0;
+
+    this.carryZanki = 0;
+    this.carrySpell = 0;
   }
 
   update() {
+    this.age++;
+    if (this.age % 60 === 0) {
+      fr = Math.floor(frameRate());
+    }
   }
 
-  debugmode() {
+  debugMode() {
   }
 
-  enddebug() {
+  endDebug() {
   }
 
-  scenectl() {
+  sceneCtl() {
+    if (this.gameSceneNow === "title") {
+      this.sceneTitle();
+    }
+    this.update();
+    if (this.gameSceneNow === "introStage1") {
+      this.introSceneStage1();
+      this.update();
+    }
+    if (this.gameSceneNow === "stage1") {
+      this.sceneStage1();
+      this.update();
+    }
   }
 
-  scenetitle() {
+  sceneTitle() {
+    background(0);
+    if (this.titleAlpha < 255) {
+      this.titleAlpha++;
+    }
+    this.titleLogo();
+    if ((this.titleAlpha === 255) && (keyIsDown(90) || mouseIsPressed)) {
+      this.gameSceneNow = "introStage1";
+      this.titleAlpha = 0;
+    }
+    if (keyIsDown(68) || touches.length === 3) {
+      this.isDebug = true;
+    }
   }
 
-  titlelogo() {
+  titleLogo() {
+    textSize(64);
+    textFont("Comic Sans MS");
+    fill(255, this.titleAlpha);
+    textAlign(CENTER);
+    text(stgTitle, canvasW / 2, canvasH / 3);
+
+    if (this.titleAlpha === 255) {
+      textSize(16);
+      textFont("Comic Sans MS");
+      fill(255);
+      textAlign(LEFT);
+      if ((this.age % 60) <= 30) {
+        if (this.isDebug) {
+          fill(255, 255, 0);
+        }
+        text("press Z to start !!", canvasW * 2 / 3, canvasH * 2 / 3);
+      }
+    }
   }
 
   gameover() {
@@ -58,19 +185,71 @@ class Game {
   stage1prologuetext() {
   }
 
-  introscenestage1() {
+  introSceneStage1() {
+    background(30);
+    if (this.intro1Alpha < 255) {
+      this.intro1Alpha++;
+    }
+    this.introSceneStage1Logo();
+    if (this.intro1Alpha === 255) {
+      this.gameSceneNow = "stage1";
+      this.intro1Alpha = 0;
+    }
   }
 
-  introscenestage1logo() {
+  introSceneStage1Logo() {
+    textSize(64);
+    textFont("Comic Sans MS");
+    fill(255, this.intro1Alpha);
+    textAlign(CENTER);
+    text("stage 1 start !!", canvasW / 2, canvasH / 3);
   }
 
-  scenestage1() {
+  sceneStage1() {
+    // stage common
+    if (this.isNeedSetup === true) {
+      this.stage1Setup();
+      this.isNeedSetup = false;
+    }
+    this.stage1Age++;
+    background(35, 25, 70);
+    stgBoard();
+    textInfo();
+    if (this.isDebug) {
+      debugTextInfo();
+    }
+    if (keyIsDown(ESCAPE)) {
+      this.sceneStack.push(this.gameSceneNow);
+      this.gameSceneNow = "pause";
+    }
+
+    // jiki proccess
+
+    // jiki bullets proccess
+
+    // enemy spawn timer proccess
+
+    // enemy proccess
+
+    // enemy bullets proccess
+
+    //stage roop end proccess
+    stgFrame();
   }
 
-  stage1setup() {
+  stage1Setup() {
+//    this.jiki = new Jiki();
   }
 
-  stage1cleanup() {
+  stage1Cleanup() {
+    this.isNeedSetup = true;
+    this.stage1Age = 0;
+    if (this.isStage1Clear) {
+      this.gameSceneNow = "introStage2";
+      this.isStage1Clear = false;
+    } else {
+      this.gameSceneNow = "gameOver";
+    }
   }
 
   getsakuraslength() {
@@ -98,16 +277,54 @@ class Game {
   }
 }
 
-function stgboard(gameage) {
+function stgBoard() {
+  push();
+  noStroke();
+  for (let i = 0; i < bg_num; i++) {
+    fill(i * 2);
+    rect(bg_x1, bg_y1 + i * bg_h, bg_w, bg_h);
+    fill(bg_num * 2 - i * 2);
+    rect(bg_x2, bg_y2 + i * bg_h, bg_w, bg_h);
+  }
+  bg_y1 += bg_spdy;
+  bg_y2 += bg_spdy;
+  if ((frameOriginY + frameH) <= bg_y1) { bg_y1 = frameOriginY - frameH }
+  if ((frameOriginY + frameH) <= bg_y2) { bg_y2 = frameOriginY - frameH }
+  pop();
 }
 
-function stgframe(gameage) {
+function stgFrame() {
+    noStroke();
+    fill(35, 25, 70);
+    rect(0, 0, frameOriginX, canvasH);  
+    rect((frameOriginX + frameW), 0, frameOriginX, canvasH);  
+    rect(0, 0, canvasW, frameOriginY);
+    rect(0, (frameOriginY + frameH), (frameOriginX + frameW), frameOriginY);  
 }
 
 function setGradient_Y(x, y, w, h, c1, c2) {
 }
 
-function textinfo() {
+function textInfo() {
+  textSize(textSize24);
+  textFont("Comic Sans MS");
+  fill(255);
+  textAlign(LEFT);
+  text(stgDifficulty, textDifficultyX, textDifficultyY);
+
+  textSize(textSize24);
+  textFont("Comic Sans MS");
+  fill(255);
+  textAlign(LEFT);
+//  text("score : " + nf(game.getjikiscore(), 7), textScoreX, textScoreY);
+  text("score : " + nf(1234, 7), textScoreX, textScoreY);
+  
+
+  textSize(textSize12);
+  textFont("Comic Sans MS");
+  fill(255);
+  textAlign(LEFT);
+  text("FPS : " + fr, textFpsX, textFpsY);
 }
 
 function debugtextinfo() {
